1b06dc4918bd49353b43aa5ccff0deb02893beb9
SumeyraSeyma <114585202+SumeyraSeyma@users.noreply.github.com>
2025-01-03 16:49:31 +0300

    login, logout and protectRoute

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -5,7 +5,7 @@ import { generateToken } from "../lib/utils.js";
 export const signup = async (req, res) => {
   const {fullName, email, password} = req.body;
   try {
-    
+
     if (!fullName || !email || !password) {
       return res.status(400).json({message: "Please fill in all fields"});
     }
@@ -47,10 +47,48 @@ export const signup = async (req, res) => {
   }
 };
 
-export const login = (req, res) => {
-  res.send("login route");
+export const login = async (req, res) => {
+  try{
+    const {email, password} = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({message: "Please fill in all fields"});
+    }
+
+    const user = await User.findOne({email});
+    if (!user) {
+      return res.status(400).json({message: "Invalid credentials"});
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(400).json({message: "Invalid credentials"});
+    }
+
+    generateToken(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    });
+    
+  } catch (error) {
+    console.error("Error in login route:", error.message);
+    res.status(500).json({message: "Server Error"});
+  }
 };
 
 export const logout = (req, res) => {
-  res.send("logout route");
+  try{
+    res.cookie("jwt", "", {maxAge: 0});
+    res.status(200).json({message: "Logged out successfully"});
+  } catch (error) {
+    console.error("Error in logout route:", error.message);
+    res.status(500).json({message: "Server Error"});
+  }
+};
+
+export const updateProfile = async (req, res) => {
 };
