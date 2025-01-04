cfdd89e5ed7a320dd0722f81330255e3e8141534
SumeyraSeyma <114585202+SumeyraSeyma@users.noreply.github.com>
2025-01-04 13:35:15 +0300

    feat: Add updateProfile and checkAuth functionality

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,22 +1,24 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import { generateToken } from "../lib/utils.js";
+import cloudinary from "./../lib/cloudinary.js";
 
 export const signup = async (req, res) => {
-  const {fullName, email, password} = req.body;
+  const { fullName, email, password } = req.body;
   try {
-
     if (!fullName || !email || !password) {
-      return res.status(400).json({message: "Please fill in all fields"});
+      return res.status(400).json({ message: "Please fill in all fields" });
     }
 
     if (password.length < 6) {
-      return res.status(400).json({message: "Password should be at least 6 characters long"});
+      return res
+        .status(400)
+        .json({ message: "Password should be at least 6 characters long" });
     }
 
-    const user = await User.findOne({email});
+    const user = await User.findOne({ email });
     if (user) {
-      return res.status(400).json({message: "User already exists"});
+      return res.status(400).json({ message: "User already exists" });
     }
 
     const salt = await bcrypt.genSalt(10);
@@ -28,7 +30,7 @@ export const signup = async (req, res) => {
       password: hashedPassword,
     });
 
-    if(newUser) {
+    if (newUser) {
       generateToken(newUser._id, res);
       await newUser.save();
 
@@ -39,30 +41,28 @@ export const signup = async (req, res) => {
         profilePic: newUser.profilePic,
       });
     }
-      
-  }
-  catch (error) {
+  } catch (error) {
     console.error("Error in signup route:", error.message);
-    res.status(500).json({message: "Server Error"});
+    res.status(500).json({ message: "Server Error" });
   }
 };
 
 export const login = async (req, res) => {
-  try{
-    const {email, password} = req.body;
+  try {
+    const { email, password } = req.body;
 
     if (!email || !password) {
-      return res.status(400).json({message: "Please fill in all fields"});
+      return res.status(400).json({ message: "Please fill in all fields" });
     }
 
-    const user = await User.findOne({email});
+    const user = await User.findOne({ email });
     if (!user) {
-      return res.status(400).json({message: "Invalid credentials"});
+      return res.status(400).json({ message: "Invalid credentials" });
     }
 
     const isPasswordCorrect = await bcrypt.compare(password, user.password);
     if (!isPasswordCorrect) {
-      return res.status(400).json({message: "Invalid credentials"});
+      return res.status(400).json({ message: "Invalid credentials" });
     }
 
     generateToken(user._id, res);
@@ -73,22 +73,52 @@ export const login = async (req, res) => {
       email: user.email,
       profilePic: user.profilePic,
     });
-    
   } catch (error) {
     console.error("Error in login route:", error.message);
-    res.status(500).json({message: "Server Error"});
+    res.status(500).json({ message: "Server Error" });
   }
 };
 
 export const logout = (req, res) => {
-  try{
-    res.cookie("jwt", "", {maxAge: 0});
-    res.status(200).json({message: "Logged out successfully"});
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logged out successfully" });
   } catch (error) {
     console.error("Error in logout route:", error.message);
-    res.status(500).json({message: "Server Error"});
+    res.status(500).json({ message: "Server Error" });
   }
 };
 
 export const updateProfile = async (req, res) => {
+  try {
+    const { profilePic } = req.body;
+    const userId = req.user._id;
+
+    if (!profilePic) {
+      return res
+        .status(400)
+        .json({ message: "Please provide a profile picture" });
+    }
+
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { profilePic: uploadResponse.secure_url },
+      { new: true }
+    );
+
+    res.status(200).json({ updatedUser });
+  } catch (error) {
+    console.error("Error in updateProfile route:", error.message);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+export const checkAuth = (req, res) => {
+  try {
+    res.status(200).json(req.user);
+  } catch (error) {
+    console.error("Error in checkAuth route:", error.message);
+    res.status(500).json({ message: "Server Error" });
+  }
 };
