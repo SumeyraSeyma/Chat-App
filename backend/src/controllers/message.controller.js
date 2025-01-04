caec51b3e63baa025ef675b71e711ca641a42567
SumeyraSeyma <114585202+SumeyraSeyma@users.noreply.github.com>
2025-01-04 13:35:44 +0300

    feat: Add message controller, routes, and models

---
+++ backend/src/controllers/message.controller.js
@@ -0,0 +1,63 @@
+import User from "../models/user.model.js";
+import Message from "../models/message.model.js";
+
+export const getUsersForSidebar = async (req, res) => {
+  try {
+    const loggeduserId = req.user._id;
+    const filteredUsers = await User.find({
+      _id: { $ne: loggeduserId },
+    }).select("-password");
+
+    res.status(200).json({ users: filteredUsers });
+  } catch (error) {
+    console.error("Error in getUsersForSidebar route:", error.message);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const myId = req.user._id;
+
+    const messages = await Message.find({
+      $or: [
+        { senderId: myId, receiverId: userToChatId },
+        { senderId: userToChatId, receiverId: myId },
+      ],
+    });
+
+    res.status(200).json({ messages });
+  } catch (error) {
+    console.error("Error in getMessages route:", error.message);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
+
+    let imageUrl = "";
+    if (image) {
+      const uploadedImage = await cloudinary.uploader.upload(image);
+      imageUrl = uploadedImage.secure_url;
+    }
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+
+    await newMessage.save();
+
+    res.status(200).json({ message: "Message Sent" });
+  } catch (error) {
+    console.error("Error in sendMessage route:", error.message);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 
 import {connectDB} from './lib/db.js';
+
+import messageRoute from './routes/message.route.js';
 import authRoutes from './routes/auth.route.js';
 
 const app = express();
@@ -14,6 +16,8 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoute);
+
 
 
 app.listen(PORT, () => {
